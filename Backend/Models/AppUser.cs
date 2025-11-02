821ef62a25f17e3365928f3f68676ae2bb6565a4
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-11-02 19:27:39 +0300

    fixed the bugs in files

--- Backend/Models/AppUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace PortfolioSites.Models;
-public class AppUser : IdentityUser
-{
-    public string? FullName { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-}
