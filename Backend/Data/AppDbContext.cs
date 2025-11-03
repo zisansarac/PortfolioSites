d87ebc67476371528be27919c526f5ddd5e5486e
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-11-03 18:45:45 +0300

    updated AppDbContext.cs

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -8,6 +8,19 @@ namespace Backend.Data;
 
 public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Post> BlogPosts => Set<Post>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Post>()
+             .HasIndex(b => b.Slug)
+             .IsUnique();
+
+        builder.Entity<Post>()
+             .HasIndex(b => b.AuthorId);
+    }   
     
 }
